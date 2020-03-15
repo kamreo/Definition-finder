a77aff38b925cd96549caf2a21059abe20cde1d3
Kamil <kamciu9@gmail.com>
2020-03-15 16:18:01 +0100

    Final console update
    Finished console version of a definition search.

--- project.py
+++ project.py
@@ -1,36 +1,44 @@
 import json
 import difflib
 from difflib import get_close_matches
+
 data=json.load(open("data.json"))
 
-def ReturnValue(key):
-    if key in data:
-        return data[key]
-    else:
-        list_of_matches=get_close_matches(key, data.keys(), 3)
-        if list_of_matches:
-            hint="Maybe you meant one of these:\n"
-            for match in list_of_matches:
-                hint+= match + "\n"
 
-            return hint
+def returnValue(key):
+    if key in data:
+        definitions=""
+        if len(data[key])>1:
+            for definition in data[key]:
+                definitions+=definition+"\n"
+
+            return definitions
+        else: return data[key][0]
+
+    else:
+        list_of_matches=get_close_matches(key, data.keys(), 1)
+        if list_of_matches:
+            print("Maybe you meant {} (y/n)".format(list_of_matches[0]))
+            answer=input().lower()
+            if(answer=="y"): return returnValue(list_of_matches[0])
+            else : return "Invalid input"
         else: return "No word like this in database."
 
 
-wannaCheck=True
+wanna_check=True
 
-while wannaCheck:
+while wanna_check:
     word = input("Enter a word you want definition for: ").lower()
     try:
-        print(ReturnValue(word))
+        print(returnValue(word))
         print("Wanna try again? (y/n)")
         answer = input().lower()
-        if answer == "n": wannaCheck = False
+        if answer == "n": wanna_check = False
 
     except:
         print("There was some issue, wanna try again? (y/n)")
         answer=input().lower()
-        if answer=="n": wannaCheck=False
+        if answer=="n": wanna_check=False
 
 
 
